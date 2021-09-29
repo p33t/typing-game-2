0088866a045066af1ad2fb524f285fdc5827d1c1
p33t <1179301+p33t@users.noreply.github.com>
2021-09-28 23:13:13 -0700

    Capture backspace keystroke

--- src/features/main/index.tsx
+++ src/features/main/index.tsx
@@ -1,6 +1,6 @@
 import {useAppDispatch, useAppSelector} from "../../app/hooks";
-import {ChangeEventHandler, KeyboardEventHandler} from "react";
-import {keyPressed, keySetChanged} from "./slice";
+import {ChangeEventHandler, KeyboardEventHandler, useMemo} from "react";
+import {backspaced, keyPressed, keySetChanged} from "./slice";
 import {Timestamper} from "../../common/timing";
 import {KeySetName, KeySetNames} from "./model";
 import KeyDefs from "./component/key-defs";
@@ -16,18 +16,36 @@ export default function MainPage() {
             keyCapture: {
                 keyDef: {
                     char: evt.key,
-                    alt: false,
-                    ctrl: false,
+                    alt: evt.altKey,
+                    ctrl: evt.ctrlKey,
                 },
                 keyedAt: timestamper()
             },
         }));
+        evt.preventDefault();
+    };
+    
+    const onKeyDown: KeyboardEventHandler = (evt) => {
+        if (evt.key === 'Backspace') {
+            dispatch(backspaced());
+            evt.preventDefault();
+        }
     };
     
     const onKeySetChange: ChangeEventHandler<HTMLSelectElement> = (evt) => {
         dispatch(keySetChanged(evt.target.value as KeySetName));
     };
 
+    const history = useMemo(() => {
+        const historyLength = main.keyHistory.length;
+        const start = Math.max(0, historyLength - 6);
+        // let start = 0;
+        // if (historyLength > 5) {
+        //     start = historyLength - 6;
+        // }
+        return main.keyHistory.slice(start).map((kc) => kc.keyDef)
+    }, [main.keyHistory])
+    
     return (<div>
         <p>Main Page: {main.config.keySetName}</p>
         <select value={main.config.keySetName} onChange={onKeySetChange}>
@@ -37,7 +55,7 @@ export default function MainPage() {
             <tbody>
             <tr>
                 <td>
-                    <KeyDefs keyDefs={main.keyHistory.map((kc) => kc.keyDef)}/>
+                    <KeyDefs keyDefs={history}/>
                 </td>
                 <td>
                     <KeyDefs keyDefs={main.keyPrompt}/>
@@ -45,10 +63,17 @@ export default function MainPage() {
             </tr>
             <tr>
                 <td>
-                    -
+                    <KeyDefs keyDefs={history}/>
                 </td>
                 <td>
-                    <input type='text' onKeyPress={onKeyPress} value={main.buffer.map((kc) => kc.keyDef.char).join()}/>
+                    {/* TODO: Not capturing ctrl+ key strokes */}
+                    <input
+                        type='text'
+                        readOnly
+                        onKeyPress={onKeyPress}
+                        onKeyDown={onKeyDown}
+                        value={main.buffer.map((kc) => kc.keyDef.char).join('')}
+                    />
                 </td>
             </tr>
             </tbody>
