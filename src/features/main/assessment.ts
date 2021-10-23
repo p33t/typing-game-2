8b95984dacf61dd60c6704199803b02a68cb379e
p33t <1179301+p33t@users.noreply.github.com>
2021-10-23 15:49:25 -0700

    Tidy a bit

--- src/features/main/assessment.ts
+++ src/features/main/assessment.ts
@@ -5,9 +5,8 @@ import {Timestamper} from "../../common/timing";
 export const PERFECT: Percent = 100;
 export const AssessmentConst = {
     PERFECT_KEY_RATE_DEFAULT: 5, // equals a score of 100% for keys per sec (default)
-//     // PERFECT_KEY_RATE_MIN: 1, // need at least one key per second as the goal
-//     // PERFECT_KEY_RATE_MAX: 8, // at most 8 key per second as the goal
-//     HISTORY_SIZE: 50, // size of sample for assessment  
+    HISTORY_SIZE_MAX: 200, // how much history to retain
+    PROMPT_SIZE: 5, // how many chars are in the prompt. Smaller is better for responsive key range setting.
     MOVING_AVERAGE_WINDOW: 4, // size of moving average window for displaying score
     INTERVAL_RANGE_FACTOR: 10, // 10 times slower than perfect is 0% (unless limit is activated)
     KEY_RANGE_GAIN_QUOTIENT: 6, // 1/x proportion of the difference will be compensated (default)
@@ -16,28 +15,10 @@ export const AssessmentConst = {
     STEADY_ACCURACY: .9 * PERFECT, // the accuracy that is deemed desirable, assuming it can be imperfect
     STEADY_SPEED: .7 * PERFECT, // the speed that is deemed desirable
 }
-//
-// export const GenerationConstant = {
-//     DISPLAY_BUFFER_SIZE: 10, // the number of chars shown to the user
-//     // EASY_KEY_TRANSITION_QUOTIENT: 8, // 1/x proportion of the range for the transition (more is steeper)
-//     // NON_REPEAT_QUOTIENT: 10, // 1/x proportion of the range for the beginning of recent use gradient
-//     // 10 = a 10th of the recent keys will be seriously improbable
-//     // NON_REPEAT_TRANSITION_QUOTIENT: 5, // 1/x proportion of the curve in the transition to fully probably recent use
-//
-//     HIGH_PROBABILITY: 20,
-// };
+
 const INTERVAL_PERFECT = 1000 / AssessmentConst.PERFECT_KEY_RATE_DEFAULT;
 const INTERVAL_0 = AssessmentConst.INTERVAL_RANGE_FACTOR * INTERVAL_PERFECT;
 const INTERVAL_RANGE = INTERVAL_0 - INTERVAL_PERFECT;
-
-export function permittedKeys(availableSorted: KeyDef[], difficulty: Percent): KeyDef[] {
-
-    let ixEnd = Math.round((availableSorted.length * difficulty / PERFECT));
-    if (ixEnd === 0) ixEnd++; // bottom out with a single element
-    if (ixEnd < availableSorted.length) return availableSorted.slice(0, ixEnd);
-
-    return availableSorted;
-}
 
 /** Assess the given key capture/prompt/interval */
 export function assess(prompt: RatedKeyDef, intervalMillis: number, keyCapture: KeyCapture): Assessment {
--- src/features/main/slice.ts
+++ src/features/main/slice.ts
@@ -145,7 +145,7 @@ function adjustDifficulty(state: MainState) {
 
 function manageKeys(state: MainState) {
     let historyLength = state.keyHistory.length;
-    while (historyLength > 100) {
+    while (historyLength > AssessmentConst.HISTORY_SIZE_MAX) {
         state.keyHistory.shift();
         historyLength--;
     }
@@ -175,7 +175,7 @@ function manageKeys(state: MainState) {
     // keep only those within difficulty range
     maxIndex = Math.round((maxIndex - 1) * state.config.keyRange / PERFECT) + 1; // min 2 chars
     const available = state.cache!.availableKeys.slice(0, maxIndex + 1);
-    while (state.keyPrompt.length < 5) {
+    while (state.keyPrompt.length < AssessmentConst.PROMPT_SIZE) {
         const next = nextKeyPrompt(available, state.keyPrompt);
         state.keyPrompt.push(next);
     }
