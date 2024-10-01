c6dd08529881e62eb17509cd5c0ddf97dce77502
p33t <p33t@localhost>
2024-09-30 23:41:16 -0700

    Update use of react
    ESM module & use createRoot() for startup

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import {Provider} from "react-redux";
 import {store} from "./app/store";
+import {createRoot} from "react-dom/client";
 
-ReactDOM.render(
+const rootElement = document.getElementById("root")!;
+const root = createRoot(rootElement);
+root.render(
   <React.StrictMode>
       <Provider store={store}>
           <App />
       </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
